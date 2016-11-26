345ebdb82ea8d3e46c139953c0f66d28322aa313
Kyle Gabriel <KyleTGabriel@gmail.com>
2016-11-26 14:57:58 -0500

    Update remote script

--- remote.py
+++ remote.py
@@ -29,20 +29,19 @@ import sys
 import time
 import RPi.GPIO as GPIO
 
-from os.path import expanduser
-home = expanduser("~")
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 lock_directory = "/var/lock"
-write_lock_file = "%s/remote-write" % lock_directory
-run_lock_file = "%s/remote-run" % lock_directory
-config_file = "%s/config/remote.cfg" % home
-
-projector_position = None
+write_lock_file = "{ldir}/remote-write".format(ldir=lock_directory)
+run_lock_file = "{ldir}/remote-run".format(ldir=lock_directory)
+config_file = os.path.join(__location__, 'remote.cfg')
 
 # GPIO pins connected to the remote buttons
 upPin = 8
 downPin = 4
-stopPin = 3
+stopPin = 7
+
 
 def usage():
     print "remote.py: Control a 3-button remote to raise and lower a" \
@@ -58,56 +57,59 @@ def usage():
     print "    -h, --help"
     print "           Show this help and exit"
 
+
 def init():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-
     GPIO.setup(upPin, GPIO.OUT)
     GPIO.output(upPin, 0)
     GPIO.setup(downPin, GPIO.OUT)
     GPIO.output(downPin, 0)
     GPIO.setup(stopPin, GPIO.OUT)
     GPIO.output(stopPin, 0)
-    
+
+
 def read_config():
-    global projector_position
     config = ConfigParser.RawConfigParser()
     config.read(config_file)
-    projector_position = config.get('Projector', 'projector_position')
-    
-def write_config():
+    return config.get('Projector', 'projector_position')
+
+
+def write_config(position):
     config = ConfigParser.RawConfigParser()
     if not os.path.exists(lock_directory):
         os.makedirs(lock_directory)    
-    lock = LockFile(write_lock_file)
-    
-    while not lock.i_am_locking():
+    write_lock = LockFile(write_lock_file)
+
+    while not write_lock.i_am_locking():
         try:
-            lock.acquire(timeout=10)
-        except:
-            print "Warning: Could not gain write lock: Breaking %s" % lock.path
-            lock.break_lock()
-            lock.acquire()
-            
+            write_lock.acquire(timeout=10)
+        except Exception as e:
+            print("Warning: Could not gain write lock: Breaking {path}:"
+                  "{err}".format(path=write_lock.path, err=e))
+            write_lock.break_lock()
+            write_lock.acquire()
+
     config.add_section('Projector')
-    config.set('Projector', 'projector_position', projector_position)
-    
+    config.set('Projector', 'projector_position', position)
+
     try:
         with open(config_file, 'wb') as configfile:
             config.write(configfile)
-    except:
-        print "Unable to write config: %s" % config_file
+    except Exception as e:
+        print("Unable to write config: {path}: {err}".format(path=config_file,
+                                                             err=e))
+    finally:
+        write_lock.release()
 
-    lock.release()
 
 def up():
-    global projector_position
     init()
     print "Raising projector screen"
     GPIO.output(upPin, 1)
     time.sleep(1)
     GPIO.output(upPin, 0)
-    time.sleep(36.5) # delay (seconds) for screen to fully raise
+    time.sleep(36.5)  # delay (seconds) for screen to fully raise
     GPIO.output(stopPin, 1)
     time.sleep(1)
     GPIO.output(stopPin, 0)
@@ -115,87 +117,83 @@ def up():
     GPIO.output(stopPin, 1)
     time.sleep(2.5)
     GPIO.output(stopPin, 0)
-    projector_position = 'up'
-    write_config()
+    write_config('up')
+
 
 def down():
-    global projector_position
     init()
     print "Lowering projector screen"
     GPIO.output(downPin, 1)
     time.sleep(1)
     GPIO.output(downPin, 0)
-    time.sleep(39) # permit catching ctrl+c to stop screen descent
+    time.sleep(39)  # permit catching ctrl+c to stop screen descent
     GPIO.output(stopPin, 1)
     time.sleep(1)
     GPIO.output(stopPin, 0)
-    projector_position = 'down'
-    write_config()
+    write_config('down')
+
+
+def write_stop():
+    init()
+    GPIO.output(stopPin, 1)
+    time.sleep(1)
+    GPIO.output(stopPin, 0)
+    write_config('stop')
+
 
 if not os.geteuid() == 0:
     sys.exit('Script must be run as root')
-    
-if len(sys.argv) == 1: # No arguments given
+
+if len(sys.argv) == 1:  # No arguments given
     usage()
     sys.exit(1)
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 'iduh',
-        ["initialize", "down", "up", "help"])
+    opts, args = getopt.getopt(sys.argv[1:],
+                               'iduh',
+                               ["initialize", "down", "up", "help"])
 except getopt.GetoptError as err:
-    print(err) # print "option -x not recognized"
+    print(err)  # print "option -x not recognized"
     usage()
     sys.exit(2)
-
 
 if not os.path.exists(lock_directory):
     os.makedirs(lock_directory)
 
-if not os.path.exists("%s/config" % home):
-    os.makedirs("%s/config" % home)
-
-lock = LockFile(run_lock_file)
-while not lock.i_am_locking():
+run_lock = LockFile(run_lock_file)
+while not run_lock.i_am_locking():
     try:
-        lock.acquire(timeout=1)
-    except:
-        print "Error: Cannot start: Run lock file present: %s" % lock.path
+        run_lock.acquire(timeout=1)
+    except Exception as e:
+        print("Error: Cannot start: Run lock file present: {path}: "
+              "{err}".format(path=run_lock.path, err=e))
         sys.exit(1)
 
 try:
-    read_config()
+    projector_position = read_config()
+    print("Projector is currently set as being {dir}.".format(
+        dir=projector_position))
     for opt, arg in opts:
         if opt in ("-i", "--initialize"):
-            print "Initializing GPIO pins connected to the projector remote"
+            print("Initializing GPIO pins connected to the projector remote")
             init()
         elif opt in ("-u", "--up"):
             if 'up' in projector_position:
-                print "Cannot raise projector screen, it's already up."
+                print("Cannot raise projector screen.")
             else:
                 up()
         elif opt in ("-d", "--down"):
             if 'down' in projector_position:
-                print "Cannot lower projector screen, it's already down."
+                print("Cannot lower projector screen.")
             else:
-                print projector_position
                 down()
-        elif opt in ("-h", "--help"):
+        else:
             usage()
 except KeyboardInterrupt:
-    init()
-    GPIO.output(stopPin, 1)
-    time.sleep(1)
-    GPIO.output(stopPin, 0)
-    projector_position = 'stop'
-    write_config()
-    print "Keyboard interrupted operation!"
-except:
-    init()
-    GPIO.output(stopPin, 1)
-    time.sleep(1)
-    GPIO.output(stopPin, 0)
-    projector_position = 'stop'
-    write_config()
-    print "Other error!"
-
-lock.release()
+    print("Keyboard interrupted operation")
+    write_stop()
+except Exception as e:
+    print("Exception: {err}".format(err=e))
+    write_stop()
+finally:
+    run_lock.release()
